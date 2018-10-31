cbf5c6f19e9ffce849371c48f556a1fe0c48326e
Cassie Moy <moy.cassie@gmail.com>
2018-10-31 00:42:08 -0400

    Use current_user helper.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id]) 
-    @attendances = Attendance.where(user: @user)
     @attendance = Attendance.new
     @attendance.user = @user
     @attendance.build_production
